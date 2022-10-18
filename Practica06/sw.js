11beb4072f0f7c6dd0bcefffeb3fb8b3404d0099
vimillan <20193tn066@utez.edu.mx>
2022-10-18 14:00:18 -0500

    feat: added practice 7th and fixed cache in practice 6th

--- Practica06/sw.js
+++ Practica06/sw.js
@@ -5,19 +5,19 @@ const STATI_CACHE_NAME = "static-cache-v1.1";
 const INMUTABLE_CACHE_NAME = "inmutable-cache-v1.1";
 const DYNAMIC_CACHE_NAME = "dynamic-cache-v1.1";
 
+const REQUEST_DATA = ["api/v2/pokemon/","/PokeAPI/sprites/", ];
 
 function cleanCache(cacheName, numberItems) {
   caches.open(cacheName).then((cache) => {
     cache.keys().then((keys) => {
       if (keys.length > numberItems) {
         cache.delete(keys[0]).then(() => {
-          cleanCache(cacheName, numberItems)
-        })
+          cleanCache(cacheName, numberItems);
+        });
       }
-    })
-  })
+    });
+  });
 }
-
 
 self.addEventListener("install", (event) => {
   console.log(INIT_MSG, "install");
@@ -29,38 +29,44 @@ self.addEventListener("install", (event) => {
       `${INIT_URL}js/main.js`,
       `${INIT_URL}css/style.css`,
       `${INIT_URL}images/Pokemon.png`,
-    ])
-  })
+    ]);
+  });
 
-  const promiseCacheInmutable = caches.open(INMUTABLE_CACHE_NAME).then((cache) => {
-    return cache.addAll([
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js',
-      'https://pokeapi.co/api/v2/pokemon/'
-    ])
-  })
+  const promiseCacheInmutable = caches
+    .open(INMUTABLE_CACHE_NAME)
+    .then((cache) => {
+      return cache.addAll([
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js",
+        "https://pokeapi.co/api/v2/pokemon/",
+      ]);
+    });
 
   event.waitUntil(Promise.all([promiseCache, promiseCacheInmutable]));
 });
 
-
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.includes('')) {
-    const resp = caches.match(event.request)
-      .then((respCache) => {
-        if (respCache) {
-          return respCache
-        }
-        return fetch(event.request).then((respWeb) => {
+  for (const data of REQUEST_DATA) {
+    let resp;
+    if (event.request.url.includes(data)) {
+       resp = fetch(event.request)
+        .then((respWeb) => {
+          if (!respWeb) {
+            return caches.match(event.request);
+          }
           caches.open(DYNAMIC_CACHE_NAME).then((chacheDynamic) => {
-            chacheDynamic.put(event.request, respWeb)
+            chacheDynamic.put(event.request, respWeb);
             cleanCache(DYNAMIC_CACHE_NAME, 20);
           });
           return respWeb.clone();
+        })
+        .catch(() => {
+          return caches.match(event.request);
         });
-      });
-    event.respondWith(resp)
-  }else {
-    event.respondWith(caches.match(event.request))
+    } else {
+      resp = caches.match(event.request);
+    }
+
+    event.respondWith(resp);
   }
 });
--- Practica07/sw.js
+++ Practica07/sw.js
@@ -5,19 +5,19 @@ const STATI_CACHE_NAME = "static-cache-v1.1";
 const INMUTABLE_CACHE_NAME = "inmutable-cache-v1.1";
 const DYNAMIC_CACHE_NAME = "dynamic-cache-v1.1";
 
+const REQUEST_DATA = [""];
 
 function cleanCache(cacheName, numberItems) {
   caches.open(cacheName).then((cache) => {
     cache.keys().then((keys) => {
       if (keys.length > numberItems) {
         cache.delete(keys[0]).then(() => {
-          cleanCache(cacheName, numberItems)
-        })
+          cleanCache(cacheName, numberItems);
+        });
       }
-    })
-  })
+    });
+  });
 }
-
 
 self.addEventListener("install", (event) => {
   console.log(INIT_MSG, "install");
@@ -25,42 +25,47 @@ self.addEventListener("install", (event) => {
     return cache.addAll([
       `${INIT_URL}`,
       `${INIT_URL}index.html`,
-      `${INIT_URL}js/app.js`,
-      `${INIT_URL}js/main.js`,
       `${INIT_URL}css/style.css`,
-      `${INIT_URL}images/Pokemon.png`,
-    ])
-  })
+    ]);
+  });
 
-  const promiseCacheInmutable = caches.open(INMUTABLE_CACHE_NAME).then((cache) => {
-    return cache.addAll([
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js',
-      'https://pokeapi.co/api/v2/pokemon/'
-    ])
-  })
+  const promiseCacheInmutable = caches
+    .open(INMUTABLE_CACHE_NAME)
+    .then((cache) => {
+      return cache.addAll([
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js",
+        "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css",
+        "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/fonts/bootstrap-icons.woff2?8d200481aa7f02a2d63a331fc782cfaf",
+        "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/fonts/bootstrap-icons.woff?8d200481aa7f02a2d63a331fc782cfaf",
+      ]);
+    });
 
   event.waitUntil(Promise.all([promiseCache, promiseCacheInmutable]));
 });
 
-
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.includes('')) {
-    const resp = caches.match(event.request)
-      .then((respCache) => {
-        if (respCache) {
-          return respCache
-        }
-        return fetch(event.request).then((respWeb) => {
+  for (const data of REQUEST_DATA) {
+    let resp;
+    if (event.request.url.includes(data)) {
+      resp = fetch(event.request)
+        .then((respWeb) => {
+          if (!respWeb) {
+            return caches.match(event.request);
+          }
           caches.open(DYNAMIC_CACHE_NAME).then((chacheDynamic) => {
-            chacheDynamic.put(event.request, respWeb)
+            chacheDynamic.put(event.request, respWeb);
             cleanCache(DYNAMIC_CACHE_NAME, 20);
           });
           return respWeb.clone();
+        })
+        .catch(() => {
+          return caches.match(event.request);
         });
-      });
-    event.respondWith(resp)
-  }else {
-    event.respondWith(caches.match(event.request))
+    } else {
+      resp = caches.match(event.request);
+    }
+
+    event.respondWith(resp);
   }
 });
