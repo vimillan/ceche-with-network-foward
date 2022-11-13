a406f4d7d7d84e01c3e9b15311d7af3841afe051
vimillan <20193tn066@utez.edu.mx>
2022-11-13 16:52:20 -0600

    feat: added card listener

--- Practica07/js/app.js
+++ Practica07/js/app.js
@@ -2,10 +2,33 @@
     navigator.serviceWorker.register('sw.js')
 }*/
 
-import { getAllNotesFire } from "./firebase-functions.js";
+import { getAllNotesFire, createNoteFire } from "./firebase-functions.js";
 
 const getAllNotes = () => {
     getAllNotesFire();
 }
 
+const saveNote = async () => {
+    const note = {
+        text: '',
+        created_at: new Date()
+    };
+    const textNote = document.getElementById('textNote');
+    note.text = textNote.value;
+
+    const generatedId = await createNoteFire(note);
+
+    if (generatedId !== 'no-created') {
+        alert('Nota creada!')
+        textNote.value = '';
+        getAllNotes();
+    } else {
+        alert('Ocurrió un error')
+    }
+}
+
 getAllNotes();
+
+const btnSave = document.getElementById('btnSave');
+btnSave.addEventListener('click', saveNote);
+
--- Practica07/js/firebase-functions.js
+++ Practica07/js/firebase-functions.js
@@ -1,19 +1,53 @@
 import { } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js"
-import { collection, query, where, getDocs, getFirestore} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"
+import { collection, query, where, getDocs, getFirestore, addDoc, limit, } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"
 import { app } from "./Firebase.js";
 
 const db = getFirestore(app);
 
 const getAllNotesFire = async () => {
-    const q = query(collection(db, "notas"));
+    const itemList = document.getElementById('itemList');
+    itemList.innerHTML = '';
+    const q = query(collection(db, "notas"), limit(5));
 
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+        const card = document.createElement('div');
+        card.classList.add('item');
+        card.classList.add('mb-2');
+        card.classList.add('mx-2');
+        card.classList.add('row');
+        card.innerHTML = `
+            <div class="col-3 col-lg-1 mt-2 mb-2">
+                <img src="https://images.unsplash.com/photo-1610126176343-3e1833a107e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                class="img-fluid rounded-circle avatar" alt="...">
+            </div>
+            <div class="col-9 col-lg-11 mt-2 mb-2">
+                <strong class="d-inline-block text-truncate" style="max-width: 200px;">
+                    Receta de pan de limón
+                </strong>
+                <br />
+                <span class="d-inline-block text-truncate" style="max-width: 200px;">
+                    ${doc.data().text}
+                </span>
+            </div>
+        `
+        card.addEventListener('click', () => {
+            console.log(doc.id)
+        })
+        itemList.appendChild(card)
     });
 }
 
-export{
-    getAllNotesFire
+const createNoteFire = async (note) => {
+    try {
+        const docRef = await addDoc(collection(db, "notas"), note);
+        return docRef.id
+    } catch (error) {
+        console.log(error)
+        return 'no-created'
+    }
+}
+
+export {
+    getAllNotesFire, createNoteFire
 }
